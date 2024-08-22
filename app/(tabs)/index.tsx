fbb679badeb415da8426a95621c81e23d0fd5a62
Kavishka Prabashara Wijerathna <kavishkaprabashara@gmail.com>
2024-08-22 22:16:39 +0530

    Other Service is grid is created

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -271,12 +271,149 @@ export default function HomeScreen() {
           Other services
         </ThemedText>
       </View>
-      <View style={styles.row} id='navBar'>
-        <ThemedText>
-          07772899876
-          {'\n'}
-          Connected
+      <View>
+        <View style={styles.gridCardContainer} id='navBar'>
+        <View style={styles.card}>
+            <Image 
+      source={require('@/assets/images/allPackagesImg/allPackagesComboDataIcon.png')} 
+      style={{ width: 50, height: 40 }} 
+      testID="allPackagesComboDataIcon.png"
+    />
+            <ThemedText style={styles.titleBar}>
+          Data packages
         </ThemedText>
+        </View>
+        <View style={styles.card}>
+        <Image 
+      source={require('@/assets/images/allPackagesImg/allPackagesComboIcon.png')} 
+      style={{ width: 50, height: 40 }} 
+      testID="allPackagesComboIcon.png"
+    />
+                <ThemedText style={styles.titleBar}>
+          Combo packages
+        </ThemedText>
+        </View>
+        <View style={styles.card}>
+        <Image 
+      source={require('@/assets/images/allPackagesImg/allPackagesVoiceIcon.png')} 
+      style={{ width: 50, height: 40 }} 
+      testID="allPackagesComboIcon.png"
+    />
+                <ThemedText style={styles.titleBar}>
+          Voice add-on
+        </ThemedText>                
+        </View>
+        <View style={styles.card}>
+        <Image 
+      source={require('@/assets/images/allPackagesImg/allPackagesVoiceIcon.png')} 
+      style={{ width: 50, height: 40 }} 
+      testID="allPackagesComboIcon.png"
+    />
+                <ThemedText style={styles.titleBar}>
+          Voice add-on
+        </ThemedText>                
+        </View>
+        <View style={styles.card}>
+        <Image 
+      source={require('@/assets/images/allPackagesImg/allPackagesVoiceIcon.png')} 
+      style={{ width: 50, height: 40 }} 
+      testID="allPackagesComboIcon.png"
+    />
+                <ThemedText style={styles.titleBar}>
+          Voice add-on
+        </ThemedText>                
+        </View>
+        <View style={styles.card}>
+        <Image 
+      source={require('@/assets/images/allPackagesImg/allPackagesVoiceIcon.png')} 
+      style={{ width: 50, height: 40 }} 
+      testID="allPackagesComboIcon.png"
+    />
+                <ThemedText style={styles.titleBar}>
+          Voice add-on
+        </ThemedText>                
+        </View>
+        <View style={styles.card}>
+        <Image 
+      source={require('@/assets/images/allPackagesImg/allPackagesVoiceIcon.png')} 
+      style={{ width: 50, height: 40 }} 
+      testID="allPackagesComboIcon.png"
+    />
+                <ThemedText style={styles.titleBar}>
+          Voice add-on
+        </ThemedText>                
+        </View>
+        <View style={styles.card}>
+        <Image 
+      source={require('@/assets/images/allPackagesImg/allPackagesVoiceIcon.png')} 
+      style={{ width: 50, height: 40 }} 
+      testID="allPackagesComboIcon.png"
+    />
+                <ThemedText style={styles.titleBar}>
+          Voice add-on
+        </ThemedText>                
+        </View>
+        <View style={styles.card}>
+        <Image 
+      source={require('@/assets/images/allPackagesImg/allPackagesVoiceIcon.png')} 
+      style={{ width: 50, height: 40 }} 
+      testID="allPackagesComboIcon.png"
+    />
+                <ThemedText style={styles.titleBar}>
+          Voice add-on
+        </ThemedText>                
+        </View>
+        <View style={styles.card}>
+        <Image 
+      source={require('@/assets/images/allPackagesImg/allPackagesVoiceIcon.png')} 
+      style={{ width: 50, height: 40 }} 
+      testID="allPackagesComboIcon.png"
+    />
+                <ThemedText style={styles.titleBar}>
+          Voice add-on
+        </ThemedText>                
+        </View>
+        <View style={styles.card}>
+        <Image 
+      source={require('@/assets/images/allPackagesImg/allPackagesVoiceIcon.png')} 
+      style={{ width: 50, height: 40 }} 
+      testID="allPackagesComboIcon.png"
+    />
+                <ThemedText style={styles.titleBar}>
+          Voice add-on
+        </ThemedText>                
+        </View>
+        <View style={styles.card}>
+        <Image 
+      source={require('@/assets/images/allPackagesImg/allPackagesVoiceIcon.png')} 
+      style={{ width: 50, height: 40 }} 
+      testID="allPackagesComboIcon.png"
+    />
+                <ThemedText style={styles.titleBar}>
+          Voice add-on
+        </ThemedText>                
+        </View>
+        <View style={styles.card}>
+        <Image 
+      source={require('@/assets/images/allPackagesImg/allPackagesVoiceIcon.png')} 
+      style={{ width: 50, height: 40 }} 
+      testID="allPackagesComboIcon.png"
+    />
+                <ThemedText style={styles.titleBar}>
+          Voice add-on
+        </ThemedText>                
+        </View>
+        <View style={styles.card}>
+        <Image 
+      source={require('@/assets/images/allPackagesImg/allPackagesVoiceIcon.png')} 
+      style={{ width: 50, height: 40 }} 
+      testID="allPackagesComboIcon.png"
+    />
+                <ThemedText style={styles.titleBar}>
+          Voice add-on
+        </ThemedText>                
+        </View>
+        </View>
       </View>
       <View>
         <ThemedText style={styles.titleBar}>
